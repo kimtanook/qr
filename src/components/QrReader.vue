c6e8aec25da35f691156ca3fae2a003d08653110
kimtanook <seob6402@gmail.com>
2024-05-15 00:28:57 +0900

    qr 테스트

--- src/components/QrReader.vue
+++ src/components/QrReader.vue
@@ -1,82 +1,181 @@
 <template>
+  <a :href="result">{{ result }}</a>
+  <div
+    v-for="item in trackFunctionOptions"
+    @click="trackFunctionSelected = item"
+  >
+    {{ item.text }}
+  </div>
   <div>
-    <video
-      class="media"
-      ref="myMediaRef"
-      id="video"
-      autoplay
-      playsinline
-    ></video>
-    <select @change="changeCamera">
-      <option>asd</option>
-      <option v-for="item in videoInput" :value="item.deviceId">
-        {{ item.label }}
-      </option>
-    </select>
+    <qrcode-stream
+      :constraints="{deviceId: selectedDevice}"
+      :track="trackFunctionSelected.value"
+      :formats="selectedBarcodeFormats"
+      @error="onError"
+      @detect="onDetect"
+      v-if="selectedDevice !== null"
+    />
+    <p v-else class="error">No cameras on this device</p>
+    <div v-for="item in cameraList" @click="selectedDevice = item.deviceId">
+      {{ item.label }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
+import {QrcodeStream} from "vue-qrcode-reader";
 
-const myStream = ref();
-const myMediaRef = ref();
-const videoInput = ref();
+const result = ref(""); // qr코드 감지 결과
 
-// 미디어 가져오기
-const getMedia = async (deviceId: string | undefined) => {
-  const initialConstrains = {
-    audio: false,
-    video: {facingMode: "user", width: 640, FrameRate: 15},
-  };
-  const cameraConstraints = {
-    audio: false,
-    video: {deviceId: {exact: deviceId}, width: 320},
-  };
+// 트랙
+const trackFunctionOptions = [
+  {text: "nothing (default)", value: undefined},
+  {text: "outline", value: paintOutline},
+  {text: "centered text", value: paintCenterText},
+  {text: "bounding box", value: paintBoundingBox},
+];
+const trackFunctionSelected = ref(trackFunctionOptions[1]); // 선택한 트랙. (바운더리, 텍스트 등)
 
-  try {
-    myStream.value = await navigator.mediaDevices.getUserMedia(
-      deviceId ? cameraConstraints : initialConstrains
-    );
+// 포맷
+const barcodeFormats: any = ref({
+  aztec: false,
+  code_128: false,
+  code_39: false,
+  code_93: false,
+  codabar: false,
+  databar: false,
+  databar_expanded: false,
+  data_matrix: false,
+  dx_film_edge: false,
+  ean_13: false,
+  ean_8: false,
+  itf: false,
+  maxi_code: false,
+  micro_qr_code: false,
+  pdf417: false,
+  qr_code: true,
+  rm_qr_code: true,
+  upc_a: false,
+  upc_e: false,
+  linear_codes: false,
+  matrix_codes: false,
+});
+const selectedBarcodeFormats = computed(() => {
+  return Object.keys(barcodeFormats.value).filter(
+    (format) => barcodeFormats.value[format]
+  );
+});
 
-    if (myMediaRef?.value) {
-      myMediaRef.value.srcObject = myStream.value;
+// 카메라
+const selectedDevice: any = ref(null);
+const devices: any = ref([]);
+const cameraList: any = ref([]);
+
+// 에러
+const error = ref("");
+
+// 감지
+function onDetect(detectedCodes: any) {
+  console.log(detectedCodes);
+  JSON.stringify(
+    detectedCodes.map((code: any) => {
+      result.value = code.rawValue;
+    })
+  );
+}
+
+// 트랙
+function paintOutline(detectedCodes: any, ctx: any) {
+  for (const detectedCode of detectedCodes) {
+    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
+
+    ctx.strokeStyle = "red";
+
+    ctx.beginPath();
+    ctx.moveTo(firstPoint.x, firstPoint.y);
+    for (const {x, y} of otherPoints) {
+      ctx.lineTo(x, y);
     }
-    if (!deviceId) {
-      await getCamera();
-    }
-  } catch (e) {
-    console.log(e);
+    ctx.lineTo(firstPoint.x, firstPoint.y);
+    ctx.closePath();
+    ctx.stroke();
   }
-};
+}
+function paintBoundingBox(detectedCodes: any, ctx: any) {
+  for (const detectedCode of detectedCodes) {
+    const {
+      boundingBox: {x, y, width, height},
+    } = detectedCode;
 
-// 카메라 가져오기
-const getCamera = async () => {
-  try {
-    const myCameras = await navigator.mediaDevices.enumerateDevices();
-    const videoInputData = myCameras.filter(
-      (item) => item.kind === "videoinput"
-    );
-    const currentCamera = myStream.value?.getVideoTracks()[0];
-    videoInput.value = videoInputData;
-  } catch (e) {
-    console.log("e : ", e);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#007bff";
+    ctx.strokeRect(x, y, width, height);
   }
-};
+}
+function paintCenterText(detectedCodes: any, ctx: any) {
+  for (const detectedCode of detectedCodes) {
+    const {boundingBox, rawValue} = detectedCode;
 
-// 카메라 변경
-const changeCamera = (e: Event) => {
-  const value = (e.target as HTMLSelectElement).value;
-  getMedia(value);
-};
+    const centerX = boundingBox.x + boundingBox.width / 2;
+    const centerY = boundingBox.y + boundingBox.height / 2;
 
-onMounted(() => {
-  getMedia("");
+    const fontSize = Math.max(12, (50 * boundingBox.width) / ctx.canvas.width);
+
+    ctx.font = `bold ${fontSize}px sans-serif`;
+    ctx.textAlign = "center";
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#35495e";
+    ctx.strokeText(detectedCode.rawValue, centerX, centerY);
+
+    ctx.fillStyle = "#5cb984";
+    ctx.fillText(rawValue, centerX, centerY);
+  }
+}
+
+// 에러 핸들링
+function onError(err: any) {
+  error.value = `[${err.name}]: `;
+
+  if (err.name === "NotAllowedError") {
+    error.value += "you need to grant camera access permission";
+  } else if (err.name === "NotFoundError") {
+    error.value += "no camera on this device";
+  } else if (err.name === "NotSupportedError") {
+    error.value += "secure context required (HTTPS, localhost)";
+  } else if (err.name === "NotReadableError") {
+    error.value += "is the camera already in use?";
+  } else if (err.name === "OverconstrainedError") {
+    error.value += "installed cameras are not suitable";
+  } else if (err.name === "StreamApiNotSupportedError") {
+    error.value += "Stream API is not supported in this browser";
+  } else if (err.name === "InsecureContextError") {
+    error.value +=
+      "Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
+  } else {
+    error.value += err.message;
+  }
+}
+
+onMounted(async () => {
+  devices.value = (await navigator.mediaDevices.enumerateDevices()).filter(
+    ({kind}) => kind === "videoinput"
+  );
+  if (devices.value.length > 0) {
+    cameraList.value = devices.value;
+    selectedDevice.value = devices.value[1].deviceId;
+  }
 });
 </script>
+
 <style scoped>
-.media {
-  width: 90vw;
-  height: 50vh;
+.error {
+  font-weight: bold;
+  color: red;
+}
+.barcode-format-checkbox {
+  margin-right: 10px;
+  white-space: nowrap;
 }
 </style>
