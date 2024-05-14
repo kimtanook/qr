c129b1ef2daf7a76eea8b2b7783f4ccc7f20f56a
kimtanook <seob6402@gmail.com>
2024-05-15 00:29:58 +0900

    qr 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import vue from "@vitejs/plugin-vue";
 import {defineConfig} from "vite";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  plugins: [
+    vue(),
+    // mkcert()
+  ],
 });
